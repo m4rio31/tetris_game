e00736eb6d686178f52aff96a3d1ccd5df418db4
Mario Lamberti <mario.lamberti@cern.ch>
2020-04-08 18:50:35 +0200

    commit javascript files

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementbyId("board");
+const ctx = canvas.getContext("2d");
+
+//************* Size of canvas ***************
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = COLS * BLOCK_SIZE;
+
+// ************ Scale blocks **************
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
