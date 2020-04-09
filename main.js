ef6cd30106b5f5d54efc87e9b262571239eb6cdf
Mario Lamberti <mario.lamberti@cern.ch>
2020-04-09 09:25:19 +0200

    center canvas

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const canvas = document.getElementbyId("board");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
 
 //************* Size of canvas ***************
 ctx.canvas.width = COLS * BLOCK_SIZE;
-ctx.canvas.height = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // ************ Scale blocks **************
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
