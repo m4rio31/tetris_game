953c4372f84a5ca979f88c1e8561561f9cdb8d44
Mario Lamberti <mario.lamberti@cern.ch>
2020-04-09 09:50:23 +0200

    create empty matrix

---
+++ board.js
@@ -0,0 +1,12 @@
+class Board {
+    grid;
+
+    reset() {
+	this.grid = this.EmptyBoard();
+    }
+
+// *********** Return a matrix of the canvas dimensions, filled with zeros************
+    EmptyBoard() {
+	return Array.from({length: ROWS}, () => Array(COLS).fill(0));
+    }
+}
