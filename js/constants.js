e7fa9f4b7cc0ea1cc7d8690a4eb7704aef3f0b86
Mario Lamberti <mario.lamberti@cern.ch>
2020-04-11 13:04:43 +0200

    fix movements

--- js/constants.js
+++ js/constants.js
@@ -15,6 +15,7 @@ const PIECES = [
 
 const KEY = {
   LEFT: 37,
+  UP: 38,
   RIGHT: 39,
   DOWN: 40
 }
--- js/main.js
+++ js/main.js
@@ -8,14 +8,6 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 // ************ Scale blocks **************
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-// *********** Blocks moves ***************
-moves = {
-  [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
-  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
-  [KEY.DOWN]: p => ({ ...p, y: p.y + 1 })
-  //[KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
-  //[KEY.UP]: p => board.rotate(p)
-};
 
 let board = new Board();
 let piece = new Piece(ctx);
@@ -30,51 +22,31 @@ let dropStart = Date.now();
 function drop() {
   let now = Date.now();
   let delta = now - dropStart;
-  if (delta > 1000){
+  if (delta > 500){
     piece.moveDown();
     dropStart = Date.now()
   }
   requestAnimationFrame(drop);
 }
 
-document.addEventListener('keydown', control)
+// ******* Declare movements for the piece ********
+document.addEventListener('keydown', movements)
 
-function control(evt) {
-  if (evt.keyCode == 37) {
+function movements(evt) {
+  if (evt.keyCode == KEY.LEFT) {
     piece.moveLeft();
     dropStart = Date.now()
   }
-  else if (evt.keyCode == 38) {
+  else if (evt.keyCode == KEY.UP) {
     piece.rotate();
     dropStart = Date.now()
   }
-  else if (evt.keyCode == 39) {
+  else if (evt.keyCode == KEY.RIGHT) {
     piece.moveRight();
     dropStart = Date.now()
   }
-  else if (evt.keyCode == 40) {
+  else if (evt.keyCode == KEY.DOWN) {
     piece.moveDown();
     dropStart = Date.now()
   }
-//    board.piece.move(p);
 }
-
-//event => {
-//  if (moves[event.keyCode]) {
-//    // Stop the event from bubbling.
-//    event.preventDefault();
-
-    // Get new state of piece
-//    let p = moves[event.keyCode](board.piece);
-
-//    if (board.valid(p)) {
-      // If the move is valid, move the piece.
-//      board.piece.move(p);
-
-      // Clear old position before drawing.
-//      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-//      board.piece.draw();
-//    }
-//  }
-//});
